fa5122023c01b7b38e53824334523a7b056d637f
Veravg <veravgelder@hotmail.com>
2016-08-12 09:51:20 +0200

    first commit

---
+++ db/migrate/20160812074617_create_news_items.rb
@@ -0,0 +1,11 @@
+class CreateNewsItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :news_items do |t|
+      t.string :title
+      t.text :sub_header
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
